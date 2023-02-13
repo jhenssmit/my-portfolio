6e67b6f8cf45e41b5429cea7d029f6ce6cb4b3a5
jhenssmit <jhenssmit@gmail.com>
2023-02-12 20:01:35 -0500

    portfolio-v1

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a156713fcd.js" crossorigin="anonymous"></script>
+    <title>Jhens Smit 😁</title>
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+        <br>
+        <p class="p-jh">Jhens Smit Abregú Llanos - Programador Junior</p>
+    </div>  
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><i class="fa-sharp fa-solid fa-house"></i> Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" aria-current="page" href="#"><i class="fa-solid fa-diagram-project"></i> Proyects</a>
+        <a class="nav-link" href="#"><i class="fa-solid fa-code"></i> Skillset</a>
+        <a class="nav-link" href="#"><i class="fa-solid fa-briefcase"></i> Experience</a>
+        <a class="nav-link" href="#"><i class="fa-solid fa-id-card-clip"></i> Contact</a>
+      </div>        
+    </div>
+    <h2>👨‍💻</h2>
+  </div>
+</nav>
+<section class="container-im">
+    <br>
+    <img src="images/gif-programacion3.gif" alt="" class="img-fo">
+    <p class="p-text">Welcome to my portfolio</p>
+</section>  
+</div>
+</body>
+</html> 
